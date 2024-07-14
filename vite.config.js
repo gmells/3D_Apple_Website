663ae449caad9a7822a3ab39732c63d009746dbb
gmells <victormells30@gmail.com>
2024-07-14 01:23:06 +0100

    change vie config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-61",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-61",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+    outDir: "dist",
+  },
+});
